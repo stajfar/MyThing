3baacea44fe1876dbefd9e790463f8d9ec173a05
stajfr <stajfar@yahoo.com>
2017-10-18 17:45:35 +0200

    drivers updated to receive pins as string

--- app/src/main/java/it/polimi/mything/MainActivity.java
+++ app/src/main/java/it/polimi/mything/MainActivity.java
@@ -1,18 +1,17 @@
 package it.polimi.mything;
 
+
 //activity default imports
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import it.polimi.myUserDrivers.GpioOut;
+import it.polimi.myUserDrivers.Bmp180;
 import it.polimi.myUserDrivers.Pcf8591;
 import it.polimi.myUserDrivers.GpioButton;
-import it.polimi.myUserDrivers.Bmp180;
 import android.util.Log;
 import android.os.Handler;
 import java.util.List;
 import java.io.IOException;
-
-
 
 
 public class MainActivity extends AppCompatActivity  {
@@ -21,15 +20,18 @@ public class MainActivity extends AppCompatActivity  {
     private static final String TAG = AppCompatActivity.class.getSimpleName();
 
     private GpioOut gpioout;
-    private static final int led1_INTERVAL = 1000;
+    private static final int led1Handler_INTERVAL = 1000;
     private Handler led1Handler = new Handler();
-    private Pcf8591 pcf8591;
-    private GpioButton gpiobutton;
     private Bmp180 bmp180;
-    private static final int pressureSensor_INTERVAL = 2000;
+    private static final int pressureSensorHandler_INTERVAL = 2000;
     private Handler pressureSensorHandler = new Handler();
-    private static final int temperatureSensor_INTERVAL = 1000;
+    private static final int temperatureSensorHandler_INTERVAL = 1000;
     private Handler temperatureSensorHandler = new Handler();
+    private Pcf8591 pcf8591;
+    private static final int analogToDigitalLightSensorHandler_INTERVAL = 1000;
+    private Handler analogToDigitalLightSensorHandler = new Handler();
+    private GpioButton gpiobutton;
+
 
 
 
@@ -41,11 +43,14 @@ public class MainActivity extends AppCompatActivity  {
 
         gpioout = new GpioOut("BCM12");
         led1Handler.post(led1HandlerRunnable);
-        pcf8591 = new Pcf8591("I2C1");
-        gpiobutton = new GpioButton("BCM26");
         bmp180 = new Bmp180("I2C1");
         pressureSensorHandler.post(pressureSensorHandlerRunnable);
         temperatureSensorHandler.post(temperatureSensorHandlerRunnable);
+        pcf8591 = new Pcf8591("I2C1");
+        analogToDigitalLightSensorHandler.post(analogToDigitalLightSensorHandlerRunnable);
+        gpiobutton = new GpioButton("BCM26");
+
+
 
 
 
@@ -60,9 +65,9 @@ public class MainActivity extends AppCompatActivity  {
 
 
 
-
-
     }
+
+
 
     private Runnable led1HandlerRunnable = new Runnable() {
         @Override
@@ -70,7 +75,7 @@ public class MainActivity extends AppCompatActivity  {
             try {
                 if (gpioout != null) {
                     //reset the handler
-                    led1Handler.postDelayed(led1HandlerRunnable, led1_INTERVAL);
+                    led1Handler.postDelayed(led1HandlerRunnable, led1Handler_INTERVAL);
                     toggleGpio();
                     Log.i(TAG, "toggleGpio: Executed."  );
 
@@ -82,14 +87,18 @@ public class MainActivity extends AppCompatActivity  {
         }
     };
 
+
+
+
     private Runnable pressureSensorHandlerRunnable = new Runnable() {
         @Override
         public void run() {
             try {
                 if (bmp180 != null) {
                     //reset the handler
-                    pressureSensorHandler.postDelayed(pressureSensorHandlerRunnable, pressureSensor_INTERVAL);
+                    pressureSensorHandler.postDelayed(pressureSensorHandlerRunnable, pressureSensorHandler_INTERVAL);
                     float readPressure = readPressure();
+                    Log.i(TAG, "readPressure: "+  readPressure );
 
                     //TODO to be extented by the developer
                 }
@@ -98,6 +107,9 @@ public class MainActivity extends AppCompatActivity  {
             }
         }
     };
+
+
+
 
     private Runnable temperatureSensorHandlerRunnable = new Runnable() {
         @Override
@@ -105,8 +117,9 @@ public class MainActivity extends AppCompatActivity  {
             try {
                 if (bmp180 != null) {
                     //reset the handler
-                    temperatureSensorHandler.postDelayed(temperatureSensorHandlerRunnable, temperatureSensor_INTERVAL);
+                    temperatureSensorHandler.postDelayed(temperatureSensorHandlerRunnable, temperatureSensorHandler_INTERVAL);
                     float readTemperature = readTemperature();
+                    Log.i(TAG, "readTemperature: "+  readTemperature );
 
                     //TODO to be extented by the developer
                 }
@@ -117,28 +130,33 @@ public class MainActivity extends AppCompatActivity  {
     };
 
 
+
+
+    private Runnable analogToDigitalLightSensorHandlerRunnable = new Runnable() {
+        @Override
+        public void run() {
+            try {
+                if (pcf8591 != null) {
+                    //reset the handler
+                    analogToDigitalLightSensorHandler.postDelayed(analogToDigitalLightSensorHandlerRunnable, analogToDigitalLightSensorHandler_INTERVAL);
+                    float readChannel = readChannel();
+                    Log.i(TAG, "readChannel: "+  readChannel );
+
+                    //TODO to be extented by the developer
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    };
+
+
+
+
+
     private void toggleGpio() throws IOException {
         ///TODO to be extented by the developer
         gpioout.toggleGpio();
-    }
-
-    private float readChannel() throws IOException {
-        return pcf8591.readChannel(0);
-
-    }
-
-    private void setOnButtonEventListener() throws IOException {
-        ///TODO to be extented by the developer
-        gpiobutton.setOnButtonEventListener(new GpioButton.onButtonEventListener() {
-
-            @Override
-            public void onButtonEvent(GpioButton gpioButton, boolean currentState)
-            {
-
-            }
-
-
-        });
     }
 
     private float readPressure() throws IOException {
@@ -149,6 +167,16 @@ public class MainActivity extends AppCompatActivity  {
     private float readTemperature() throws IOException {
         return bmp180.readTemperature();
 
+    }
+
+    private float readChannel() throws IOException {
+        return pcf8591.readChannel(0);
+
+    }
+
+    private void setOnButtonEventListener() throws IOException {
+        ///TODO to be extented by the developer
+        gpiobutton.setOnButtonEventListener(new GpioButton.onButtonEventListener() {    @Override            public void onButtonEvent(GpioButton gpioButton, boolean currentState) {                            }        });
     }
 
 
@@ -179,6 +207,16 @@ public class MainActivity extends AppCompatActivity  {
             e.printStackTrace();
         }
         try {
+            pressureSensorHandler.removeCallbacks(pressureSensorHandlerRunnable);
+            temperatureSensorHandler.removeCallbacks(temperatureSensorHandlerRunnable);
+
+            bmp180.close();
+            bmp180 = null;
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        try {
+            analogToDigitalLightSensorHandler.removeCallbacks(analogToDigitalLightSensorHandlerRunnable);
 
             pcf8591.close();
             pcf8591 = null;
@@ -189,15 +227,6 @@ public class MainActivity extends AppCompatActivity  {
 
             gpiobutton.close();
             gpiobutton = null;
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        try {
-            pressureSensorHandler.removeCallbacks(pressureSensorHandlerRunnable);
-            temperatureSensorHandler.removeCallbacks(temperatureSensorHandlerRunnable);
-
-            bmp180.close();
-            bmp180 = null;
         } catch (IOException e) {
             e.printStackTrace();
         }
